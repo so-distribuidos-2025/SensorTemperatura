78ed3071286502bd7bce20e34f809abc564dd004
brunardo19 <balderramab19@gmail.com>
2025-10-10 16:59:54 -0300

    Añadido enviroment variables para kubernetes

--- src/main/java/com/mycompany/SensorTemperatura/SensorTemperatura.java
+++ src/main/java/com/mycompany/SensorTemperatura/SensorTemperatura.java
@@ -18,12 +18,12 @@ import java.util.logging.Logger;
  * Clase principal SensorTemperatura
  * ---------------------------------
  * Este programa representa el cliente que simula un sensor de temperatura.
- * 
+ * <p>
  * Funcionalidades:
- *  - Establece una conexión con un servidor en un puerto específico.
- *  - Envía un mensaje inicial identificándose como "temperatura".
- *  - Inicia un hilo (HiloSensado) que genera y transmite valores de
- *    temperatura de forma periódica.
+ * - Establece una conexión con un servidor en un puerto específico.
+ * - Envía un mensaje inicial identificándose como "temperatura".
+ * - Inicia un hilo (HiloSensado) que genera y transmite valores de
+ * temperatura de forma periódica.
  */
 public class SensorTemperatura {
 
@@ -38,12 +38,17 @@ public class SensorTemperatura {
         PrintWriter pw;                  // Flujo de salida para enviar datos
 
         try {
-            // Obtiene la dirección IP del servidor (localhost en este caso)
-            ipServidor = InetAddress.getByName("localhost");
-
-            // Se conecta al servidor en el puerto 20000
-            Socket cliente = new Socket(ipServidor, 20000);
-            System.out.println(cliente);
+            String controladorHost = System.getenv("CONTROLADOR_HOST");
+            if (controladorHost == null) {
+                controladorHost = "localhost";
+            }
+            String controladorPort = System.getenv("CONTROLADOR_PORT");
+            if (controladorPort == null) {
+                controladorPort = "20000";
+            }
+            ipServidor = InetAddress.getByName(controladorHost);
+            Socket cliente = new Socket(ipServidor, Integer.parseInt(controladorPort));
+            System.out.println("Conectado al servidor: " + cliente);
 
             // Prepara el PrintWriter con autoflush activado para enviar datos
             pw = new PrintWriter(cliente.getOutputStream(), true);
@@ -54,19 +59,28 @@ public class SensorTemperatura {
             // Crea e inicia el hilo que simula el sensor de temperatura
             HiloSensado sensor = new HiloSensado(cliente, pw);
 
-            int port = 22000;
-            String name = "SensorTemperaturaRMI";
             sensor.start();
 
             //Clase unicast para mandar los datos
             HiloServerRMI hiloServerRMI = new HiloServerRMI(sensor);
 
             try {
-                // Crea el registro RMI en el puerto calculado
-                LocateRegistry.createRegistry(port);
+                String name = "SensorTemperaturaRMI";
+                String sensorHostname = System.getenv("HOSTNAME");
+                if (sensorHostname == null) {
+                    sensorHostname = "localhost";
+                }
+                String envPort = System.getenv("PORT");
+                if (envPort == null) {
+                    envPort = "22000";
+                }
+                int sensorPort = Integer.parseInt(envPort);
+
+                // Crea el registro RMI para la consola
+                LocateRegistry.createRegistry(sensorPort);
 
                 // Publica el servidor remoto en el registro
-                Naming.rebind("rmi://localhost:"+port+"/"+name, hiloServerRMI);
+                Naming.rebind("rmi://" + sensorHostname + ":" + sensorPort + "/" + name, hiloServerRMI);
 
             } catch (RemoteException ex) {
                 Logger.getLogger(HiloServerRMI.class.getName()).log(Level.SEVERE,
